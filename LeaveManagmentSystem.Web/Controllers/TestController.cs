59ba89038a8f9b50e46736fa253b332400da11f3
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-10-06 19:15:32 +0300

    Add project files.

---
+++ LeaveManagmentSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using LeaveManagmentSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagmentSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(2000,05,09)
+
+            };
+
+            return View(data);
+        }
+    }
+}
