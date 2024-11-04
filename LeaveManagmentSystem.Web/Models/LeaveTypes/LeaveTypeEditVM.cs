645fd66d3728757a0134719b3a8f6807d0151a7a
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-11-04 06:52:59 +0200

    Add ViewModel

---
+++ LeaveManagmentSystem.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
@@ -0,0 +1,14 @@
+﻿using LeaveManagmentSystem.Web.Models.LeaveTypes;
+using System.ComponentModel.DataAnnotations;
+
+public class LeaveTypeEditVM : BaseLeaveTypeVM
+{
+    [Required]
+    [Length(4, 150, ErrorMessage = "You have violated the length requirement")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, 90)]
+    [Display(Name = "Maximum Allocation of Days")]
+    public int NumberOfDays { get; set; }
+}
